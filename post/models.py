cf2ed11996ad4e1d0b1d11889f2bc55792019665
izzyjosh <calebayuba222@gmail.com>
2023-12-28 16:51:02 +0100

    Added an app for handling post

--- post/models.py
+++ post/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Post(models.Model):
+    post_owner = models.ForeignKey(User,on_delete=models.CASCADE,related_name="post_owner")
+    caption = models.CharField(max_length=3000,blank=True,null=True)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ["-date_posted"]
+
+    def __str__(self):
+        return self.post_owner.username
+
+
+
--- post/views.py
+++ post/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import HttpRequest
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def home(request:HttpRequest):
+    return render(request,"post/home.html")
