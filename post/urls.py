0feb89f907e46cf2d451513342105f5f31a8e033
izzyjosh <calebayuba222@gmail.com>
2023-12-30 14:51:47 +0100

    Implemented the follow system and created the notification model

--- post/urls.py
+++ post/urls.py
@@ -6,4 +6,7 @@ app_name = "post"
 urlpatterns = [
         path("",views.home,name="home"),
         path("post/",views.post,name="post"),
+        path("follows/",views.follow,name="follow"),
+        path("profile/<uuid:user_id>/",views.profile,name="user_profile"),
+        path("addfollow/<uuid:user_id>/",views.addfollow,name="addfollow"),
         ]
--- post/views.py
+++ post/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render,redirect
-from .models import Post
+from .models import Post,Notification
 from django.http import HttpRequest
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+
+
+User= get_user_model()
 
 @login_required
 def home(request:HttpRequest):
@@ -23,3 +27,45 @@ def post(request:HttpRequest):
                 )
         return redirect("post:home")
     return render(request,"post/post.html")
+
+
+def follow(request:HttpRequest):
+    if request.method == "POST":
+        search = request.POST.get("search")
+        friends = User.objects.filter(username__icontains=search)
+
+    else:
+        friends = User.objects.exclude(username=request.user.username)
+    context = {"follows":friends}
+    return render(request,"post/follow.html",context)
+
+
+def profile(request:HttpRequest,user_id):
+    user = User.objects.get(id=user_id)
+    return render(request,"post/profile.html",{"user":user})
+
+
+def addfollow(request:HttpRequest,user_id):
+    following = User.objects.get(id=user_id)
+    current_user = request.user
+
+    status = request.POST.get("follow")
+    if status == "follow":
+        current_user.follows.add(following)
+
+        Notification.objects.create(
+                user=current_user,
+                receiver=following,
+                content=f"{current_user} just followed you")
+
+    elif status == "unfollow":
+        current_user.follows.remove(following)
+
+        Notification.objects.create(
+                user=current_user,
+                receiver=following,
+                content=f"{current_user} just Unfollowed you")
+
+    current_user.save()
+
+    return redirect("post:follow")
